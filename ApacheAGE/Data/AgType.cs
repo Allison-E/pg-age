e55da77c3ea632c93e899fbf7cc44f0247443d54
Emmanuel Allison <emmallison13@gmail.com>
2023-09-10 03:42:22 +0100

    Add project files.

---
+++ ApacheAGE/Data/AgType.cs
@@ -0,0 +1,50 @@
+﻿namespace ApacheAGE.Data;
+
+/// <summary>
+/// Represents the data type returned by AGE.
+/// </summary>
+/// <remarks>
+/// Given the type parameter <typeparamref name="T"/>, an implicit conversion
+/// will be tried from <see cref="object"/> to <typeparamref name="T"/> for
+/// <see cref="AgType{T}.Value"/>.
+/// </remarks>
+/// <typeparam name="T">
+/// </typeparam>
+internal class AgType<T>
+{
+    /// <summary>
+    /// Data value.
+    /// </summary>
+    public T Value { get; }
+
+    /// <summary>
+    /// Creates a new instance of <see cref="AgType{}"/>.
+    /// </summary>
+    /// <param name="value">
+    /// Value to store in the instance.
+    /// </param>
+    /// <remarks>
+    /// An implicit conversion will be tried to convert the input value
+    /// to type <typeparamref name="T"/> before storing it.
+    /// </remarks>
+    public AgType(object value)
+    {
+        Value = (T)value;
+    }
+}
+
+/// <summary>
+/// Represents the data type returned by AGE.
+/// </summary>
+internal class AgType: AgType<object>
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="AgType"/>.
+    /// </summary>
+    /// <param name="value">
+    /// Value to store in the instance.
+    /// </param>
+    public AgType(object value) : base(value)
+    {
+    }
+}
---
+++ ApacheAGE/IAgeClient.cs
@@ -0,0 +1,110 @@
+﻿namespace ApacheAGE;
+
+/// <summary>
+/// Defines clients for use with the Apache AGE extension for PostgreSQL.
+/// </summary>
+internal interface IAgeClient
+{
+    /// <summary>
+    /// Connect to the database.
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task"/> for monitoring the progress of the operation.
+    /// </returns>
+    Task ConnectAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Create a graph.
+    /// </summary>
+    /// <param name="graphName">
+    /// Graph name.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns></returns>
+    Task CreateGraphAsync(
+        string graphName,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Disconnect from the database.
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task"/> for monitoring the progress of the operation.
+    /// </returns>
+    Task DisconnectAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Drop the given graph.
+    /// </summary>
+    /// <param name="graphName">
+    /// Graph name.
+    /// </param>
+    /// <param name="cascade">
+    /// Indicates that labels and data that depend on the graph should
+    /// be deleted. Default is <see langword="false"/>.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns></returns>
+    Task DropGraphAsync(
+        string graphName,
+        bool cascade = false,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Execute the given cypher query.
+    /// </summary>
+    /// <remarks>
+    /// Only cypher queries currently supported by Apache AGE will run
+    /// successfully; others will throw an error.
+    /// </remarks>
+    /// <param name="graphName">
+    /// Graph name.
+    /// </param>
+    /// <param name="cypherQuery">
+    /// Cypher quer.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns>
+    /// The result as <see cref="AgType"/>.
+    /// </returns>
+    Task<AgType> ExecuteCypherAsync(
+        string graphName,
+        string cypherQuery,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Execute the given cypher query.
+    /// </summary>
+    /// <remarks>
+    /// Only cypher queries currently supported by Apache AGE will run
+    /// successfully; others will throw an error.
+    /// </remarks>
+    /// <param name="graphName">
+    /// Graph name.
+    /// </param>
+    /// <param name="cypherQuery">
+    /// Cypher quer.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// Token for propagating a notification to stop the running operation.
+    /// </param>
+    /// <returns>
+    /// The result converted to <see cref="AgType{T}"/>.
+    /// </returns>
+    Task<AgType<T>> ExecuteCypherAsync<T>(
+        string graphName,
+        string cypherQuery,
+        CancellationToken cancellationToken = default);
+}
