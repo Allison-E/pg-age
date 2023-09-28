882a806e747c77342ab69a2bbac5c7663875e0d0
Emmanuel Allison <emmallison13@gmail.com>
2023-09-28 18:15:50 +0100

    feat: edge and vertex data types

---
+++ src/ApacheAGE/Data/Edge.cs
@@ -0,0 +1,32 @@
+﻿namespace ApacheAGE.Data;
+
+/// <summary>
+/// AGE edge.
+/// </summary>
+public class Edge
+{
+    /// <summary>
+    /// GraphId for the edge.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// GraphId for the source vertex.
+    /// </summary>
+    public int StartId { get; set; }
+
+    /// <summary>
+    /// GraphId for the target vertex.
+    /// </summary>
+    public int EndId { get; set; }
+
+    /// <summary>
+    /// Name of the label for the edge.
+    /// </summary>
+    public string? Label { get; set; }
+
+    /// <summary>
+    /// Edge's properties.
+    /// </summary>
+    public Dictionary<string, object?>? Properties { get; set; }
+}
---
+++ src/ApacheAGE/Data/Vertex.cs
@@ -0,0 +1,22 @@
+﻿namespace ApacheAGE.Data;
+
+/// <summary>
+/// AGE vertex.
+/// </summary>
+public class Vertex
+{
+    /// <summary>
+    /// GraphId for the vertex.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Name of the label for this vertex.
+    /// </summary>
+    public string? Label { get; set; }
+
+    /// <summary>
+    /// Vertex's properties.
+    /// </summary>
+    public Dictionary<string, object?>? Properties { get; set; }
+}
